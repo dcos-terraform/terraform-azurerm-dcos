e3b910e1c6f279fe09d039d01878f592c6c675c5
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-25 13:49:42 +0200

    chore: updated variable descriptions with tfdescsan

--- outputs_infra_module.tf
+++ outputs_infra_module.tf
@@ -8,16 +8,6 @@ output "infrastructure.masters.os_user" {
   value       = "${module.dcos-infrastructure.masters.os_user}"
 }
 
-output "infrastructure.forwarding_rules.masters" {
-  description = "Master Forwarding Rules"
-  value       = "${module.dcos-infrastructure.forwarding_rules.masters}"
-}
-
-output "infrastructure.forwarding_rules.public_agents" {
-  description = "Public Agent Forwarding Rules"
-  value       = "${module.dcos-infrastructure.forwarding_rules.public_agents}"
-}
-
 output "infrastructure.private_agents.os_user" {
   description = "Private Agent instances private OS default user"
   value       = "${module.dcos-infrastructure.private_agents.os_user}"
@@ -38,11 +28,6 @@ output "infrastructure.bootstrap.public_ip" {
   value       = "${module.dcos-infrastructure.bootstrap.public_ip}"
 }
 
-output "infrastructure.masters.zone_list" {
-  description = "Master instances zone list"
-  value       = "${module.dcos-infrastructure.masters.zone_list}"
-}
-
 output "infrastructure.masters.public_ips" {
   description = "Master instances public IPs"
   value       = "${module.dcos-infrastructure.masters.public_ips}"
@@ -51,16 +36,6 @@ output "infrastructure.masters.public_ips" {
 output "infrastructure.masters.private_ips" {
   description = "Master instances private IPs"
   value       = "${module.dcos-infrastructure.masters.private_ips}"
-}
-
-output "infrastructure.masters.subnetwork_name" {
-  description = "Master instances subnetwork name"
-  value       = "${module.dcos-infrastructure.masters.subnetwork_name}"
-}
-
-output "infrastructure.private_agents.zone_list" {
-  description = "Private Agent zone list"
-  value       = "${module.dcos-infrastructure.private_agents.zone_list}"
 }
 
 output "infrastructure.private_agents.public_ips" {
@@ -73,16 +48,6 @@ output "infrastructure.private_agents.private_ips" {
   value       = "${module.dcos-infrastructure.private_agents.private_ips}"
 }
 
-output "infrastructure.private_agents.subnetwork_name" {
-  description = "Private Agent instances subnetwork name"
-  value       = "${module.dcos-infrastructure.private_agents.subnetwork_name}"
-}
-
-output "infrastructure.public_agents.zone_list" {
-  description = "Public Agent zone list"
-  value       = "${module.dcos-infrastructure.public_agents.zone_list}"
-}
-
 output "infrastructure.public_agents.public_ips" {
   description = "Public Agent public IPs"
   value       = "${module.dcos-infrastructure.public_agents.public_ips}"
@@ -93,12 +58,17 @@ output "infrastructure.public_agents.private_ips" {
   value       = "${module.dcos-infrastructure.public_agents.private_ips}"
 }
 
-output "infrastructure.public_agents.subnetwork_name" {
-  description = "Public Agent instances subnetwork name"
-  value       = "${module.dcos-infrastructure.public_agents.subnetwork_name}"
+output "infrastructure.vnet_id" {
+  description = "ID of the VNET"
+  value       = "${module.dcos-infrastructure.vnet_id}"
 }
 
-output "infrastructure.network_self_link" {
+output "infrastructure.vnet_name" {
+  description = "Name of the VNET"
+  value       = "${module.dcos-infrastructure.vnet_id}"
+}
+
+output "infrastructure.resource_group_name" {
   description = "Self link of created network"
-  value       = "${module.dcos-infrastructure.network_self_link}"
+  value       = "${module.dcos-infrastructure.resource_group_name}"
 }
