320af960e3f20dd0d73f3a590f78af733270d120
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-27 20:25:40 +0200

    added more outputs

--- outputs_infra_module.tf
+++ outputs_infra_module.tf
@@ -28,6 +28,11 @@ output "infrastructure.bootstrap.public_ip" {
   value       = "${module.dcos-infrastructure.bootstrap.public_ip}"
 }
 
+output "infrastructure.bootstrap.nsg_id" {
+  description = "Network security group id of the bootstrap instance"
+  value       = "${module.dcos-infrastructure.bootstrap.nsg_id}"
+}
+
 output "infrastructure.masters.public_ips" {
   description = "Master instances public IPs"
   value       = "${module.dcos-infrastructure.masters.public_ips}"
@@ -36,6 +41,11 @@ output "infrastructure.masters.public_ips" {
 output "infrastructure.masters.private_ips" {
   description = "Master instances private IPs"
   value       = "${module.dcos-infrastructure.masters.private_ips}"
+}
+
+output "infrastructure.masters.nsg_id" {
+  description = "Network security group id of the master instances"
+  value       = "${module.dcos-infrastructure.masters.nsg_id}"
 }
 
 output "infrastructure.private_agents.public_ips" {
@@ -48,6 +58,11 @@ output "infrastructure.private_agents.private_ips" {
   value       = "${module.dcos-infrastructure.private_agents.private_ips}"
 }
 
+output "infrastructure.private_agents.nsg_id" {
+  description = "Network security group id of the private agent instances"
+  value       = "${module.dcos-infrastructure.private_agents.nsg_id}"
+}
+
 output "infrastructure.public_agents.public_ips" {
   description = "Public Agent public IPs"
   value       = "${module.dcos-infrastructure.public_agents.public_ips}"
@@ -56,6 +71,11 @@ output "infrastructure.public_agents.public_ips" {
 output "infrastructure.public_agents.private_ips" {
   description = "Public Agent instances private IPs"
   value       = "${module.dcos-infrastructure.public_agents.private_ips}"
+}
+
+output "infrastructure.public_agents.nsg_id" {
+  description = "Network security group id of the public agent instances"
+  value       = "${module.dcos-infrastructure.public_agents.nsg_id}"
 }
 
 output "infrastructure.vnet_id" {
@@ -71,4 +91,9 @@ output "infrastructure.vnet_name" {
 output "infrastructure.resource_group_name" {
   description = "Name of the resource group"
   value       = "${module.dcos-infrastructure.resource_group_name}"
+}
+
+output "infrastructure.subnet_id" {
+  description = "Subnet ID of the infrastructure"
+  value       = "${module.dcos-infrastructure.subnet_id}"
 }
